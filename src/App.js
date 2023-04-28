35c99a346eed923b061e8b2afb6c69da7939ea1e
abi-manoharan97 <abishekh.manoharan@gmail.com>
2023-04-28 12:25:42 -0400

    added filter by importance capability

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState("")
+  const [filter, setFilter] = useState(true)
 
   const handleNewNoteSubmit = (e) => {
     e.preventDefault()
     const aNote = {
       id: notes[notes.length-1].id+1,
       content: newNote,
-      important: Math.random() < .50,
+      important: Math.random() < 0.5,
     }
     setNotes(notes.concat(aNote))
-    
+
   }
 
   const handleNoteChange = (e) => {
@@ -21,17 +22,32 @@ const App = (props) => {
     setNewNote(e.target.value)
   }
 
+  const filtered = notes.filter(note => 
+    note.important === true
+  )  
+  
+  const handleFilterClick = () => {
+    setFilter(!filter)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <form onSubmit={handleNewNoteSubmit}>
         <input onChange={handleNoteChange} name="note"/>
+        <input type='button' onClick={handleFilterClick} value="filter"/>
         <button type='submit'>Submit</button>  
       </form>
       <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        
+        {filter ? 
+          notes.map(note => 
+            <Note key={note.id} note={note} />
+          ):
+          filtered.map(note => 
+            <Note key={note.id} note={note} />
+          )
+        }
       </ul>
     </div>
   )
